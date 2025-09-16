7553c51030a27952392dbdd27ce5081a9b970cff
Akash S <akashspalloor@gmail.com>
2025-09-16 12:44:22 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import connectDB from './config/db.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(hpp());
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+}));
+app.use(cookieParser());
+
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('ShopyZone API is running...');
+});
+
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Start server after DB connection
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch(err => {
+    console.error('Failed to connect to the database', err);
+});
