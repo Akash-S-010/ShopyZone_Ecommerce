28768aab76c162753f2ccb446a3849e206b18cf5
Akash S <akashspalloor@gmail.com>
2025-09-20 17:36:10 +0530

    updated import errors

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { addReview, getReviews, deleteReview } from '../controllers/reviewController.js';
 import { checkSeller } from '../middlewares/sellerMiddleware.js';
-import { checkSellerOrAdmin } from '../middlewares/selerOrAdminMiddleware.js';
+import { checkAuth } from '../middlewares/authMiddleware.js';
+import { checkSellerOrAdmin } from '../middlewares/sellerOrAdminMiddleware.js';
 
 const router = express.Router();
 
@@ -11,7 +13,7 @@ router.put('/:id', checkSeller, updateProduct);
 router.delete('/:id', checkSellerOrAdmin, deleteProduct);
 
 // Public routes (or user-accessible routes)
-router.get('/', getProducts);
+router.get('/', getAllProducts);
 router.get('/:id', getProductById);
 
 // Review routes
